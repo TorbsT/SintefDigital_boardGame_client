ad8d83f262766df0557f474fa6b6084314d9d768
TorbsT <torbst2001@gmail.com>
2023-02-19 16:15:58 +0900

    add TempNode

---
+++ Assets/Scripts/View/ViewController/TempNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace View
+{
+    public class TempNode : MonoBehaviour, INode
+    {
+        [SerializeField] private List<TempNode> neighbours = new();
+        public ICollection<INode> GetNeighbours()
+        {
+            List<INode> result = new();
+            foreach (INode node in neighbours)
+            {
+                result.Add(node);
+            }
+            return result;
+        }
+    }
+}
