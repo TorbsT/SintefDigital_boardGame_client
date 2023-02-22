bdf08060bec8c890ececb76f723cd1605dc5d6e0
Mathias Gunnarshaug <mathg@stud.ntnu.no>
2023-02-22 14:36:49 +0100

    finished movement, and node highlight

--- Assets/nodeTraversal.cs
+++ Assets/nodeTraversal.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class nodeTraversal : MonoBehaviour
+public class NodeTraversal : MonoBehaviour
 {
+    public GameObject player;
+
     public GameObject[] neighbourNodes;
+
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
+
     void Start()
     {
         //Debug.Log(GameObject.Find("gameBoard/CitySquare").transform.GetSiblingIndex());
         //Debug.Log(GameObject.Find("gameBoard").transform.GetChild(2).GetType());
         //Debug.Log(transform.GetSiblingIndex());
-        Steps steps = GetComponent<Steps>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,4 +25,30 @@ public class nodeTraversal : MonoBehaviour
     {
         
     }
+
+    private void OnMouseEnter()
+    {
+        if (neighbourNodes.Contains(player.transform.parent.gameObject))
+        {
+            //Debug.Log("COLOR!!!");
+            spriteRenderer.color = Color.cyan;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+            //Debug.Log("Reset!!!");
+            spriteRenderer.color = Color.white;
+    }
+
+    void OnMouseDown()
+    {
+        if (neighbourNodes.Contains(player.transform.parent.gameObject))
+        {
+            //Debug.Log("clicked");
+            player.transform.parent = transform;
+            player.transform.localPosition = new Vector3(0, 0, 0);
+        }
+    }
+
 }
