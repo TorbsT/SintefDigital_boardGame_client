e18573569c74de5d27a28e48c9d312575ce83775
TorbsT <torbst2001@gmail.com>
2023-02-26 21:29:28 +0900

    add login menu, main menu

---
+++ Assets/Scripts/View/ViewController/NodeTraversal.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static Network.GameState;
+
+namespace View
+{
+    public class NodeTraversal : MonoBehaviour, INode
+    {
+        public GameObject player;
+        public GameObject[] neighbourNodes;
+        private SpriteRenderer spriteRenderer;
+        // Start is called before the first frame update
+        void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        void Start()
+        {
+            //Debug.Log(GameObject.Find("gameBoard/CitySquare").transform.GetSiblingIndex());
+            //Debug.Log(GameObject.Find("gameBoard").transform.GetChild(2).GetType());
+            //Debug.Log(transform.GetSiblingIndex());
+            Steps steps = GetComponent<Steps>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        public ICollection<INode> GetNeighbours()
+        {
+            List<INode> result = new();
+            foreach (GameObject neighbourNode in neighbourNodes)
+            {
+                if (neighbourNode == null) continue;
+                result.Add(neighbourNode.GetComponent<INode>());
+            }
+            return result;
+        }
+        private void OnMouseEnter()
+        {
+            if (neighbourNodes.Contains(player.transform.parent.gameObject))
+            {
+                //Debug.Log("COLOR!!!");
+                spriteRenderer.color = Color.cyan;
+            }
+        }
+
+        private void OnMouseExit()
+        {
+            //Debug.Log("Reset!!!");
+            spriteRenderer.color = Color.white;
+        }
+
+        void OnMouseDown()
+        {
+            if (neighbourNodes.Contains(player.transform.parent.gameObject))
+            {
+                //Debug.Log("clicked");
+                player.transform.parent = transform;
+                player.transform.localPosition = new Vector3(0, 0, 0);
+            }
+        }
+    }
+}
