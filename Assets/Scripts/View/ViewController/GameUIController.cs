f63ee1e62c78cf559ef463e79d287a5f0d05e958
TorbsT <torbst2001@gmail.com>
2023-03-05 16:04:28 +0900

    add pause screen

---
+++ Assets/Scripts/View/ViewController/GameUIController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace View
+{
+    public class GameUIController : MonoBehaviour
+    {
+        [SerializeField] private string disconnectToScene;
+        [SerializeField] private KeyCode pauseKey = KeyCode.Escape;
+        [SerializeField] private GameObject pauseMenu;
+        private void Awake()
+        {
+            
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(pauseKey))
+                pauseMenu.SetActive(!pauseMenu.activeSelf);
+        }
+        public void DisconnectClicked()
+        {
+            SceneManager.LoadSceneAsync(disconnectToScene);
+        }
+        public void ContinueClicked()
+        {
+            pauseMenu.SetActive(false);
+        }
+    }
+}
+
