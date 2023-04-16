ece0bedacbd560cb950118ea0c944713814f09a8
TorbsT <torbst2001@gmail.com>
2023-04-16 19:23:39 +0200

    color scheme system, new font

--- Assets/Scripts/View/ViewController/UIListHandler.cs
+++ Assets/Scripts/View/ViewController/UIListHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace View
 {
@@ -7,6 +8,12 @@ namespace View
     {
         [SerializeField] private RectTransform content;
         private List<GameObject> items = new();
+        private VerticalLayoutGroup layoutGroup;
+
+        private void OnEnable()
+        {
+            layoutGroup = content.GetComponent<VerticalLayoutGroup>();
+        }
         public void AddItem(GameObject gameObject)
         {
             RectTransform newLobbyTransform = gameObject.GetComponent<RectTransform>();
@@ -15,15 +22,16 @@ namespace View
                 Vector2.down * content.sizeDelta;
             newLobbyTransform.sizeDelta = new(1f, newLobbyTransform.sizeDelta.y);
             content.sizeDelta +=
-                Vector2.up * newLobbyTransform.sizeDelta.y;
+                Vector2.up * (newLobbyTransform.sizeDelta.y+layoutGroup.spacing);
             items.Add(gameObject);
         }
         public void Clear()
         {
+            if (layoutGroup == null) layoutGroup = content.GetComponent<VerticalLayoutGroup>();
             foreach (GameObject gameObject in items)
                 PoolManager.Instance.Enpool(gameObject);
             items = new();
-            content.sizeDelta = new(content.sizeDelta.x, 0f);
+            content.sizeDelta = new(content.sizeDelta.x, layoutGroup.padding.vertical-layoutGroup.spacing);
         }
     }
 }
