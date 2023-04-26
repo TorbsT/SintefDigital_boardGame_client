578d1017601b1eb33f6959bf4ed941f4fb41d939
TorbsT <torbst2001@gmail.com>
2023-04-26 13:17:23 +0200

    start scale components

---
+++ Assets/Scripts/View/ViewController/UIScaleComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace View
+{
+    internal class UIScaleComponent : MonoBehaviour
+    {
+        public enum Direction
+        {
+            Vertical,
+            Horizontal
+        }
+        [field: SerializeField] public Direction ExpandDirection { get; private set; }
+        [field: SerializeField, Range(0f, 2f)] public float ExpandScalar = 1f;
+        private RectTransform rectTransform;
+        private Vector2 originalSize;
+
+        private void OnEnable()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            originalSize = rectTransform.sizeDelta;
+            Debug.Log(originalSize);
+        }
+
+    }
+}
