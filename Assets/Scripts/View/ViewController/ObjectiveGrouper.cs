d581cc4d5bdf854deb7347fc35cc84f9736d99a6
TorbsT <torbst2001@gmail.com>
2023-05-03 14:24:05 +0200

    wip unstack markers

---
+++ Assets/Scripts/View/ViewController/ObjectiveGrouper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace View
+{
+    internal class ObjectiveGrouper : MonoBehaviour
+    {
+        public static ObjectiveGrouper Instance { get; private set; }
+        [SerializeField] private float groupDistance = 0.5f;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+
+    }
+}
